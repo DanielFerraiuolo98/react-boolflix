0aadf4c5150e99a2c0b8b97da1394d12d0122893
DanielFerraiuolo98 <daniel.ferraiuolo.98@gmail.com>
2025-01-14 16:37:26 +0100

    aggiunta della card per le serie tv più altre regole CSS

--- react-boolflix/src/CardSeries.jsx
+++ react-boolflix/src/CardSeries.jsx
@@ -1,16 +1,17 @@
 import axios from 'axios';
 import { useState, useEffect } from 'react';
 
-function Card() {
+function CardSeries() {
     const [data, setData] = useState([]);
-    const apiUrlFilm = "https://api.themoviedb.org/3/search/movie?api_key=70f7f9ddab19c38e31331a2bc333f7ca&query=a&language=it-IT";
+
+    const apiUrlSeries = "https://api.themoviedb.org/3/search/tv?api_key=70f7f9ddab19c38e31331a2bc333f7ca&query=a&language=it-IT"
 
     useEffect(() => {
-        axios.get(apiUrlFilm).then((res) => {
+        axios.get(apiUrlSeries).then((res) => {
             setData(res.data.results);
         })
             .catch((error) => {
-                console.log(error, "errore nella ricerca dei film");
+                console.log(error, "errore nella ricerca della serie TV");
             })
     }, []);
 
@@ -19,9 +20,9 @@ function Card() {
             <ul>
                 {data.map((item, index) => (
                     <li key={index}>
-                        <h2>{item.title}</h2>
-                        <img src={`https://image.tmdb.org/t/p/w342${item.poster_path}`} alt={item.title} />
-                        <h3>{item.original_title}</h3>
+                        <h2>{item.name}</h2>
+                        <img src={`https://image.tmdb.org/t/p/w342${item.poster_path}`} alt={item.name} />
+                        <h3>{item.original_name}</h3>
                         <p className='language'>{item.original_language}</p>
                         <p className='voto'>{item.vote_average}</p>
                     </li>
@@ -29,7 +30,6 @@ function Card() {
             </ul>
         </>
     )
-
 }
 
-export default Card;
+export default CardSeries;
--- react-boolflix/src/components/Main.jsx
+++ react-boolflix/src/components/Main.jsx
@@ -1,14 +1,21 @@
 import Card from "../Card";
+import CardSeries from "../CardSeries";
 
 function Main() {
     return (
         <>
             <main>
                 <div className="movies">
+                    <div className="container-film">
+                        <h5 className="film">Movies</h5>
+                    </div>
                     <Card />
                 </div>
                 <div className="series">
-
+                    <div className="container-serie">
+                        <h5 className="serie">Series</h5>
+                    </div>
+                    <CardSeries />
                 </div>
             </main>
         </>
